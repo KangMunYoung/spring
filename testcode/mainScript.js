c27acc0fa0a8dbd95588df2f16f5e186e4a939d6
DESKTOP-4ON3QDN\KANG <kangtb321@gmail.com>
2021-09-26 18:20:09 +0900

    프로젝트 화면 구성중

--- testcode/mainScript.js
+++ testcode/mainScript.js
@@ -1,11 +1,44 @@
+/**
+ * 회원가입 모달창 관련 스크립트
+ */
+const openJoinBotton = document.getElementById('joinBtn'); //회원가입 버튼
+const nextJoinBotton1 = document.getElementById('joinNextBtn'); // 회원가입 다음버튼 1
+const cancelJoinBtton = document.getElementById('joinCancelBtn'); // 취소버튼
+const loginJoinModal = document.querySelector('.loginJoinModal');
 
-document.getElementById("joinModal_close_btn").onclick = function() {
-    document.getElementById("joinModal").style.display="none";
-}   
+const modalOverlay = loginJoinModal.querySelector('.modalOverlay');
 
-function join(){
-  document.getElementById("joinModal").style.display="block"
-}
-function joinNext(){
-  document.getElementById("joinModal").style.display="none";
-}
+//모달창 실행
+const openJoinModal = function () {
+  loginJoinModal.classList.remove('modalHidden');
+};
+
+//취소 클릭 이벤트
+const nextJoinModal1 = function () {
+  loginJoinModal.classList.add('modalHidden');
+};
+
+const closeJoinModal = function () {
+  loginJoinModal.classList.add('modalHidden');
+};
+//회원가입 다음버튼 누르면 동작 실행 바꿔야함!!!!!!!!!!!!!!!!!
+cancelJoinBtton.addEventListener('click', nextJoinModal1);
+
+// 모달창 종료이벤트 실행
+modalOverlay.addEventListener('click', closeJoinModal);
+nextJoinBotton1.addEventListener('click', closeJoinModal);
+
+//회원가입창 클릭시 모달창 오픈
+openJoinBotton.addEventListener('click', openJoinModal);
+
+//체크박스 전부 선택
+$(document.querySelector('.allJoinCheckbox')).ready(function () {
+  $('.allJoinCheckbox').click(function () {
+    $('.joinCheckbox').prop('checked', this.checked);
+  });
+});
+
+//오버레이 클릭시 모달창 닫기
+$('.modalOverlay').on(function () {
+  $(this).addClass('.modalHidden');
+});
